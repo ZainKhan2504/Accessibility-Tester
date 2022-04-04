45032193c79f40a04f8aaf4c062aa8bd17825761
zaikh847 <zaikh847@student.liu.se>
2022-04-04 18:15:23 +0500

    Add cors for origin issue

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const pa11y = require("pa11y");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// CORS
+app.use(cors());
 
 app.use(express.static("public"));
 
