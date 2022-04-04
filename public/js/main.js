244953a3d8b6866618c422681ab25e95aef890d7
zaikh847 <zaikh847@student.liu.se>
2022-04-04 17:48:38 +0500

    Add issues to DOM

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,24 @@ const testAccessibility = async (e) => {
 
 // Add issues to DOM
 const addIssuesToDOM = (issues) => {
-  console.log(issues);
+  const issuesOutput = document.querySelector("#issues");
+  issuesOutput.innerHTML = "";
+  if (issues.length === 0) {
+    issuesOutput.innerHTML = "<h4>No Issues Found</h4>";
+  } else {
+    issues.forEach((issue) => {
+      const output = `
+          <div class="card mb-5">
+            <div class="card-body">
+                <h4>${issue.message}</h4>
+                <p class="bg-light p-3 my 3">${escapeHTML(issue.context)}</p>
+                <p class="bg-secondary text-light p-2">CODE: ${issue.code}</p>
+            </div>
+          </div>
+          `;
+      issuesOutput.innerHTML += output;
+    });
+  }
 };
 
 // Set loading state
@@ -35,5 +52,13 @@ const setLoading = (isLoading = true) => {
 };
 
 // Escape HTML
+const escapeHTML = (html) => {
+  return html
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+};
 
 document.querySelector("#form").addEventListener("submit", testAccessibility);
