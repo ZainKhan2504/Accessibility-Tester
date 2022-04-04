5630c70e50c6da54a2be7017306a58f03b3f2a0a
zaikh847 <zaikh847@student.liu.se>
2022-04-04 16:53:38 +0500

    Create Backend API

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const express = require("express");
 const pa11y = require("pa11y");
 
-const run = async () => {
-  const response = await pa11y("https://zainalikhan.netlify.app/");
+const PORT = process.env.PORT || 5000;
 
-  console.log(response);
-};
+const app = express();
 
-run();
+app.use(express.static("public"));
+
+app.get("/api/test", async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: "URL is required" });
+  } else {
+    const results = await pa11y(req.query.url);
+    res.status(200).json(results);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
